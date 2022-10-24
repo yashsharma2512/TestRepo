0a02f2f503127e0666f8baa9b281eabf3a9fce43
yashsharma2512 <zackgotyorback@gmail.com>
2022-10-24 14:42:07 +0530

    fingers detection

--- test2.py
+++ test2.py
@@ -1,5 +1,19 @@
-a = 10
-b = 11
+import cv2 as cv
+from cvzone.HandTrackingModule import HandDetector
+# from math import atan
+import math
 
-add = a+b
-print(add)
+cap = cv.VideoCapture(0)
+
+detector = HandDetector(detectionCon=0.8,minTrackCon=0.8)
+
+while True:
+    ret,frame = cap.read()
+    hands,img  = detector.findHands(frame)
+    if hands:
+        left = hands[0]
+        lmlist = left['lmList']
+        bbox = left['bbox']
+        fingers = detector.fingersUp(left)
+        angle = math.degrees()
+
